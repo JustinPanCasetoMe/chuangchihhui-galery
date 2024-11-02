3e8374cd11e734e81d39c51fd74d399c293138f6
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-02 20:26:10 +0800

    Update Experiences pages three tab

--- ChuangChihHui-Galery-Main/src/App.jsx
+++ ChuangChihHui-Galery-Main/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
 
       <Header />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
--- ChuangChihHui-Galery-Main/src/components/Footer.jsx
+++ ChuangChihHui-Galery-Main/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='df aln-itm-c fw pd-x-container'>
+      <p className='fw txt-aln-c' style={{fontSize:'12px'}}>&copy;莊志輝藝術作品.保留所有權利</p>
+    </footer>
   )
 }
 
