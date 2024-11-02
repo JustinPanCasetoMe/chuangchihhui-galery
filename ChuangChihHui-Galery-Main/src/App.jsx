522419c7a20a625f14f7e66cab86a1f2a395b276
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-03 02:59:58 +0800

    Update Portfolio page Artworks

--- ChuangChihHui-Galery-Main/src/App.jsx
+++ ChuangChihHui-Galery-Main/src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Route, Routes } from 'react-router'
-import { Portfolio, Experiences, Critics, Collections, Contacts, Home } from './pages'
+import { Portfolio, Experiences, Critics, Collections, Contacts, Home, Artworks } from './pages'
 import exhibition from './datas/exhibition.json'
 import competition from './datas/competition.json'
 import archive from './datas/archive.json'
+import artworks from './datas/artworks.json'
 import 'slick-carousel/slick/slick.css';
 import "slick-carousel/slick/slick-theme.css";
 
@@ -15,8 +16,8 @@ function App() {
     <div className=''>
 
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/portfolio' element={<Portfolio />} />
         <Route 
           path='/experiences'
           element={
@@ -24,10 +25,11 @@ function App() {
               exhibition={exhibition}
               competition={competition}
               archive={archive}
-            />}></Route>
-        <Route path='/critics' element={<Critics />}></Route>
-        <Route path='/collections' element={<Collections />}></Route>
-        <Route path='/contacts' element={<Contacts />}></Route>
+            />}/>
+        <Route path='/critics' element={<Critics />} />
+        <Route path='/collections' element={<Collections/>} />
+        <Route path='/contacts' element={<Contacts />} />
+        <Route path='/portfolio/:artworksName' element={<Artworks artworks={artworks}/>} />
       </Routes>
 
 
---
+++ ChuangChihHui-Galery-Main/src/pages/Artworks.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom';
+
+const Artworks = ({ artworks = [] }) => {
+
+    //Get the artwork name from the Json file
+    // const { artworkName } = useParams();
+    const [artworkName, setArtworkName] = useState('')
+
+    //Find the artwork in the artworks array
+    // const artwork = artworks.find(item => item.name === artworkName)
+
+    const handlePickArtwork = (artworkPicked) => {
+        setArtworkName(artworkName === artworkPicked ? null : artworkPicked)
+    }
+
+
+    const main_artworksRender = artworks.map((artwork, index)=> {
+        if(artwork.name === artworkName){
+            return(
+                <div key={index} className='df jc-sb fw mg-b-lg'>
+                    <div className='df jc-c' style={{height:'500px'}}>
+                        <img src={artwork.img} alt="" className='bd-r-sm bx-sd-sm fw'/>
+                    </div>
+    
+                    <div className='df fd-c jc-c' style={{width:'40%'}}>
+    
+                        <h3 className='mg-b-30'>{artwork.name}</h3>
+                        <ul className='mg-b-30'>
+                            <span className='df'>
+                                <li className='mg-b-5'>尺寸：</li>
+                                <li className='lt-sc'>{artwork.size}</li>
+                            </span>
+                            <span className='df'>
+                                <li className='mg-b-5'>媒材：</li>
+                                <li className='lt-sc'>{artwork.mediums}</li>
+                            </span>
+                            <span className='df'>
+                                <li className='mg-b-5'>年份：</li>
+                                <li className='lt-sc'>{artwork.year}</li>
+                            </span>
+                        </ul>
+                        
+                        <button className='trans-2'>同期作品</button>
+                    </div>
+                </div>
+            )
+        }
+        console.log("artwork.name = ", artwork.name)
+        console.log("artworkName = ", artworkName)
+    })
+
+    const simillar_artworksRender = artworks.map((artwork, index) => {
+        if(artwork.id != 1){
+            return(
+                <div key={index} className='' style={{height:'160px'}}>
+
+                    {/* Creating a link to the artwork detail page*/}
+                    <Link to={`/portfolio/artwork_${artwork.name}`} onClick={ () => handlePickArtwork(`${artwork.name}`)}>
+                        <img src={artwork.img} alt="" className='bd-r-sm bx-sd-sm fh mg-r-20 imgActive trans-2'/>
+                    </Link>
+
+                </div>
+            )
+        }
+    })
+
+    return (
+        <div>
+            {main_artworksRender}
+            <div>
+                <h2 className='mg-b-30'>相似作品</h2>
+                <div className='df fl-wp'>
+                    {simillar_artworksRender}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Artworks
