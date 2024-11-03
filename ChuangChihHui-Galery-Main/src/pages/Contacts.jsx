56d75ced731d7884ed598669030cbedca3bebf1d
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-04 00:07:54 +0800

    Update Contacts Page

--- ChuangChihHui-Galery-Main/src/pages/Contacts.jsx
+++ ChuangChihHui-Galery-Main/src/pages/Contacts.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div className='fh df jc-c aln-itm-c'>
+      <div>
+        <h3>經紀人：楊素敏小姐</h3>
+        <h3>聯絡電話：０９１１８６８６４９</h3>
+        <h3>工作室地址：新竹市東區學府路１９號</h3>
+        <h3>EMAIL：gmail.com</h3>
+      </div>
+    </div>
   )
 }
 
