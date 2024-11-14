f1b3ddfddb4bd39d661599c58ec6f17f4dbe76c8
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-14 22:46:50 +0800

    Artworks Periods Slider for imgs Pages Update

--- ChuangChihHui-Galery-Main/src/pages/Periods/Period_1986_2001.jsx
+++ ChuangChihHui-Galery-Main/src/pages/Periods/Period_1986_2001.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css';
+import "slick-carousel/slick/slick-theme.css";
 
 const Period_1986_2001 = ({ artworks = [] }) => {
+
+  const ScreenWidth = window.innerWidth
+
+  var settingsSlideOne = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  var settingsSlideTwo = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
 
 
   const imgRender = (img_Id, row) => {
@@ -15,13 +36,18 @@ const Period_1986_2001 = ({ artworks = [] }) => {
           <img
             src={artworks[img_Id].img} alt="" 
             className='bx-sd-sm imgActive trans-2 mg-b-50'
-            style={{height: 
-              row === 1 ? '450px' :
-              row === 2 ? '400px' :
-              row === 3 ? '320px' :
-              row === 4 ? '360px' :
-              row === 5 ? '360px' :
-              row === 6 ? '800px' : ''
+            style={{
+              height: 
+                row === 1 ? '450px' :
+                row === 2 ? '400px' :
+                row === 3 ? '320px' :
+                row === 4 ? '360px' :
+                row === 5 ? '360px' :
+                (375<=ScreenWidth && ScreenWidth < 425) && row === 6 ? '300px' : '',
+              width:
+                (375<=ScreenWidth && ScreenWidth < 425) && row === 2 ? '100%' : 
+                (375<=ScreenWidth && ScreenWidth < 425) && row === 3 ? '100%' : 
+                (375<=ScreenWidth && ScreenWidth < 425) && row === 4 ? '100%' : ''
             }}
           />
         </Link>
@@ -37,7 +63,8 @@ const Period_1986_2001 = ({ artworks = [] }) => {
   return (
 
     <>
-      <div>
+      <div className={`${(1024<=ScreenWidth && ScreenWidth < 1960) ? '' : 'dn'} `}>
+      
         <h2 className='txt-aln-c' style={{marginBottom:'200px'}}>1986-2001</h2>
 
         <div className='df jc-c'>
@@ -63,6 +90,114 @@ const Period_1986_2001 = ({ artworks = [] }) => {
           {imgRender(22, 5)}
           {imgRender(23, 5)}
           {imgRender(24, 5)}
+        </div>
+        <div className='df jc-c'>
+          {imgRender(25, 6)}
+        </div>
+      </div>
+
+      <div className={`${(768<=ScreenWidth && ScreenWidth < 1024) ? '' : 'dn'} `}>
+
+        <h2 className='txt-aln-c' style={{marginBottom:'200px'}}>1986-2001</h2>
+
+        <div className='df jc-c'>
+          {imgRender(14, 1)}
+        </div>
+        <div className='df jc-sa'>
+          {imgRender(15, 2)}
+          {imgRender(16, 2)}
+        </div>
+        <div className='df jc-sb'>
+          {imgRender(17, 3)}
+          {imgRender(2, 3)}
+          {imgRender(3, 3)}
+        </div>
+        <div className='df jc-sb'>
+          {imgRender(1,4)}
+          {imgRender(18,4)}
+          {imgRender(19,4)}
+          {imgRender(20,4)}
+        </div>
+        <div className='df jc-sa'>
+          {imgRender(21, 5)}
+          {imgRender(22, 5)}
+          {imgRender(23, 5)}
+          {imgRender(24, 5)}
+        </div>
+        <div className='df jc-c'>
+          {imgRender(25, 6)}
+        </div>
+      </div>
+
+      <div className={`${(425<=ScreenWidth && ScreenWidth < 768) ? '' : 'dn'} `}>
+
+        <h2 className='txt-aln-c' style={{marginBottom:'200px'}}>1986-2001</h2>
+
+        <div className='df jc-c'>
+          {imgRender(14, 1)}
+        </div>
+        <div className='df jc-sa'>
+        <Slider {...settingsSlideOne}>
+          {imgRender(15, 2)}
+          {imgRender(16, 2)}
+        </Slider>
+        </div>
+        <div className='df jc-sb'>
+          {imgRender(17, 3)}
+          {imgRender(2, 3)}
+          {imgRender(3, 3)}
+        </div>
+        <div className='df jc-sb'>
+          {imgRender(1,4)}
+          {imgRender(18,4)}
+          {imgRender(19,4)}
+          {imgRender(20,4)}
+        </div>
+        <div className='df jc-sa'>
+          {imgRender(21, 5)}
+          {imgRender(22, 5)}
+          {imgRender(23, 5)}
+          {imgRender(24, 5)}
+        </div>
+        <div className='df jc-c'>
+          {imgRender(25, 6)}
+        </div>
+      </div>
+
+      <div className={`${(375<=ScreenWidth && ScreenWidth < 425) ? '' : 'dn'} `}>
+        <h2 className='txt-aln-c' style={{marginBottom:'200px'}}>1986-2001</h2>
+
+        <div className='df jc-c'>
+          {imgRender(14, 1)}
+        </div>
+        <div className=''>
+          <Slider {...settingsSlideOne}>
+            {imgRender(15, 2)}
+            {imgRender(16, 2)}
+          </Slider>
+        </div>
+        <div className=''>
+          <Slider {...settingsSlideOne}>
+            {imgRender(17, 3)}
+            {imgRender(2, 3)}
+            {imgRender(3, 3)}
+          </Slider>
+        </div>
+        <div className=''>
+          <Slider {...settingsSlideOne}>
+            {imgRender(1,4)}
+            {imgRender(18,4)}
+            {imgRender(19,4)}
+            {imgRender(20,4)}
+          </Slider>
+        </div>
+        <div className=''>
+          <Slider {...settingsSlideTwo}>
+            {imgRender(21, 5)}
+            {imgRender(22, 5)}
+            {imgRender(23, 5)}
+            {imgRender(24, 5)}
+          </Slider>
         </div>
         <div className='df jc-c'>
           {imgRender(25, 6)}
