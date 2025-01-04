b18645231c69e7ce7ac98c6fccd72d9c3d5b9ee8
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-01-04 22:35:26 +0800

    Translation Contact Update

--- ChuangChihHui-Galery-Main/src/pages/Contacts.jsx
+++ ChuangChihHui-Galery-Main/src/pages/Contacts.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../i18n'
+import { useTranslation } from 'react-i18next'
 
 const Contacts = () => {
+
+  const { t, i18n } = useTranslation()
+
   return (
     <div>
-      <h1 className='fw-b' style={{marginBottom:'20px'}}>聯繫我們</h1>
-      <h1 className='fw-b' style={{marginBottom:'80px'}}>歡迎合作邀約</h1>
+      <h1 className='fw-b' style={{marginBottom:'20px'}}>{t('聯繫我們')}</h1>
+      <h1 className='fw-b' style={{marginBottom:'80px'}}>{t('歡迎合作邀約')}</h1>
       <div style={{marginBottom:'80px'}}>
-        <h3 className='mg-b-20'>經紀人：楊素敏小姐</h3>
-        <h3 className='mg-b-20'>聯絡電話：０９１１８６８６４９</h3>
-        <h3 className='mg-b-20'>工作室地址：新竹市東區學府路１９號</h3>
-        <h3 className='mg-b-20'>EMAIL：yamifilm@gmail.com</h3>
+        <h3 className='mg-b-20'>{t('經紀人：楊素敏小姐')}</h3>
+        <h3 className='mg-b-20'>{t('聯絡電話：０９１１８６８６４９')}</h3>
+        <h3 className='mg-b-20'>{t('工作室地址：新竹市東區學府路１９號')}</h3>
+        <h3 className='mg-b-20'>{t('EMAIL：yamifilm@gmail.com')}</h3>
       </div>
       {/* <div className='df jc-c' style={{marginBottom:'120px'}}>
         <div className='fh df jc-sb aln-itm-c' style={{width:'1000px'}}>
@@ -33,10 +38,10 @@ const Contacts = () => {
       </div> */}
       <div>
         <ul className='df'>
-          <a href='https://maps.app.goo.gl/A4tttWxRBfXKYTRf8' className='outerLink mg-r-30' target='_blank'><li>地址</li></a>
-          <a href='https://www.instagram.com/sawago88/' className='outerLink mg-r-30' target='_blank'><li>IG</li></a>
-          <a href='https://www.facebook.com/chc888' className='outerLink mg-r-30' target='_blank'><li>FB</li></a>
-          <a href='https://youtu.be/aSooyTSjAh8?si=kG7R3wzFHLUqbYqg' className='outerLink mg-r-30' target='_blank'><li>紀錄片</li></a>
+          <a href='https://maps.app.goo.gl/A4tttWxRBfXKYTRf8' className='outerLink mg-r-30' target='_blank'><li>{t('地址')}</li></a>
+          <a href='https://www.instagram.com/sawago88/' className='outerLink mg-r-30' target='_blank'><li>{t('IG')}</li></a>
+          <a href='https://www.facebook.com/chc888' className='outerLink mg-r-30' target='_blank'><li>{t('FB')}</li></a>
+          <a href='https://youtu.be/aSooyTSjAh8?si=kG7R3wzFHLUqbYqg' className='outerLink mg-r-30' target='_blank'><li>{t('紀錄片')}</li></a>
         </ul>
       </div>
     </div>
