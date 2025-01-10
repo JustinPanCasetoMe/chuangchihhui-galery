652af6897f51c763a15d617c2538f3f0892f7a60
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-01-10 22:21:49 +0800

    Home page media button

--- ChuangChihHui-Galery-Main/src/pages/Home.jsx
+++ ChuangChihHui-Galery-Main/src/pages/Home.jsx
@@ -34,14 +34,12 @@ const Home = () => {
       </div>
 
       <div className={`${(375<=ScreenWidth && ScreenWidth < 425) ? '' : 'dn'}`}>
-        {/* <div>
-            <ul className='df'>
-              <a href='https://maps.app.goo.gl/A4tttWxRBfXKYTRf8' className='outerLink mg-r-30' target='_blank'><li>地址</li></a>
-              <a href='https://www.instagram.com/sawago88/' className='outerLink mg-r-30' target='_blank'><li>IG</li></a>
-              <a href='https://www.facebook.com/chc888' className='outerLink mg-r-30' target='_blank'><li>FB</li></a>
-              <a href='https://youtu.be/aSooyTSjAh8?si=kG7R3wzFHLUqbYqg' className='outerLink mg-r-30' target='_blank'><li>紀錄片</li></a>
-            </ul>
-          </div> */}
+        <ul className='txt-aln-c'>
+          <a href='https://maps.app.goo.gl/A4tttWxRBfXKYTRf8' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>地址</li></a>
+          <a href='https://www.instagram.com/sawago88/' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>IG</li></a>
+          <a href='https://www.facebook.com/chc888' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>FB</li></a>
+          <a href='https://youtu.be/aSooyTSjAh8?si=kG7R3wzFHLUqbYqg' style={{width:'100%'}} className='outerLink mg-r-30 mg-b-20 db' target='_blank'><li>紀錄片</li></a>
+        </ul>
       </div>
 
     </div>
